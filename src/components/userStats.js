96699e1087d1d8c17e695e7ca5101f12255e6e84
ruizaj13 <ruizaj13@gmail.com>
2021-05-12 15:23:17 -0700

    temp fix for 404

--- src/components/userStats.js
+++ src/components/userStats.js
@@ -1,4 +1,4 @@
-import React, { useEffect }  from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Styled from 'styled-components';
 import DoughnutChart from './Charts/DoughnutChart';
@@ -6,7 +6,7 @@ import GitHubCalendar from 'react-github-calendar';
 import SideBar from './SideBar/SideBar';
 import RepoCards from './RepoCards/RepoCards';
 import ReactTooltip from 'react-tooltip';
-import { useHistory } from 'react-router-dom';
+import NotFound from './NotFound';
 
 
 const StatCard = Styled.img`
@@ -20,35 +20,34 @@ font-family:'Share Tech';
 
 
 const UserStats = ({ user, error }) => {
-    const { push } = useHistory()
-
-    useEffect(() => {
-        if (error.message === 'Request failed with status code 404' ) {
-            push('/NotFound')
-        }
-    }, [error.message, push])
-
+    console.log(user.login)
+    console.log(error.message)
 
     return (
         <>
-            <SideBar/>
-            <DoughnutChart/>
-            <GitHubCalendar username={user.login} color="hsl(203, 82%, 33%)" Tooltips='true' fontSize= {16} blockSize={17} blockMargin={3} style={{
-                width:'75%', 
-                marginLeft:'23%', 
-                marginTop:'1%', 
-                paddingLeft:'3.8%', 
-                paddingTop:'1.1%', 
-                paddingBottom:'0.1%', 
-                backgroundColor:'rgba(255, 255, 255, 0.9)', 
-                border: '2px solid rgba(0, 0, 0, 0.1)',
-                borderRadius: '10px',
-                fontFamily:'Share Tech'
-                }}>
-                <ReactTooltip delayShow={50} html />
-            </GitHubCalendar>
-            <StatCard  alt="github stats" src={`https://readme-stats-eta-three.vercel.app/api?username=${user.login}&bg_color=00000000&show_icons=true&include_all_commits=true&count_private=true`} /> 
-            <RepoCards/>    
+            {error === 'Request failed with status code 404' ?
+                <NotFound/> :
+                <>
+                <SideBar/>
+                <DoughnutChart/>
+                <GitHubCalendar username={user.login} color="hsl(203, 82%, 33%)" Tooltips='true' fontSize= {16} blockSize={17} blockMargin={3} style={{
+                    width:'75%', 
+                    marginLeft:'23%', 
+                    marginTop:'1%', 
+                    paddingLeft:'3.8%', 
+                    paddingTop:'1.1%', 
+                    paddingBottom:'0.1%', 
+                    backgroundColor:'rgba(255, 255, 255, 0.9)', 
+                    border: '2px solid rgba(0, 0, 0, 0.1)',
+                    borderRadius: '10px',
+                    fontFamily:'Share Tech'
+                    }}>
+                    <ReactTooltip delayShow={50} html />
+                </GitHubCalendar>
+                <StatCard  alt="github stats" src={`https://readme-stats-eta-three.vercel.app/api?username=${user.login}&bg_color=00000000&show_icons=true&include_all_commits=true&count_private=true`} /> 
+                <RepoCards/>
+                </>
+            }    
         </>
     )
 }
