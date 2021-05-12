8f67d36050906e17d9102e2dd7b93556e7b5d18a
ruizaj13 <ruizaj13@gmail.com>
2021-05-12 13:30:00 -0700

    vercel

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
-import { useHistory } from 'react-router-dom'
 
 export const FETCH_USER = 'FETCH_USER'
 export const FETCHING_ERROR = 'FETCHING_ERROR'
 
 export const getUser = userName => dispatch => {
-    const {push} = useHistory()
 
     axios
         .get(`https://api.github.com/users/${userName}`)
@@ -14,7 +12,6 @@ export const getUser = userName => dispatch => {
         })
         .catch( err => {
             dispatch({ type: FETCHING_ERROR, payload: err.message})
-            push('/NotFound')
             
         })
 }
